5603bf9dd413c175e1dc171776132beb81d6cb11
MarkdeBrunner <246325@edu.p.lodz.pl>
2026-02-01 13:34:03 +0100

    Dodano import funkcji mediany do testów jednostkowych

--- src/tests/math.test.js
+++ src/tests/math.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { mode, stdDev, to4 } from '../lib/math'
+import { mode, stdDev, to4, median} from '../lib/math'
 
 describe('to4 rounding', () => {
   it('rounds down at 5th decimal below 5', () => {
